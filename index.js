38c2fd984b3b7edf9d64460ff0e72e36d2b6d660
EKhanfar97 <146889083+EKhanfar97@users.noreply.github.com>
2024-03-01 15:41:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var test = document.querySelector('div');
+test.classList;
+test.classList.add('demo');
+
+var myElement = document.querySelector('h1');
+myElement.classList;
+myElement.classList.remove('vip');
